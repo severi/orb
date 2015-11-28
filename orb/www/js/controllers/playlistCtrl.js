c4cb303654c7593130bfb55179f320d1fce9b7b0
Severi Haverila <severi.haverila@gmail.com>
2015-11-28 16:10:51 +0200

    initial project structure

---
+++ orb/www/js/controllers/playlistCtrl.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('starter.controllers').controller('PlaylistCtrl', ['$scope',
+  function($scope) {
+  }
+]);
---
+++ orb/www/js/controllers/playlistsCtrl.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+angular.module('starter.controllers').controller('PlaylistsCtrl', ['$scope',
+  function($scope) {
+    // This provides Authentication context.
+    $scope.playlists = [
+      { title: 'Reggae', id: 1 },
+      { title: 'Chill', id: 2 },
+      { title: 'Dubstep', id: 3 },
+      { title: 'Indie', id: 4 },
+      { title: 'Rap', id: 5 },
+      { title: 'Cowbell', id: 6 }
+    ];
+  }
+]);
