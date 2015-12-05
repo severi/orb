7b4bcd14bdf8cc7d9e443e323dffdfff06ece5b1
Sakari Pesonen <pesonen.sakari@gmail.com>
2015-12-05 02:01:30 +0200

    siitä sitä herkkua

--- orb/www/js/controllers/orbView.controller.js
+++ orb/www/js/controllers/orbView.controller.js
@@ -7,11 +7,12 @@ function OrbViewController($scope, $ionicPlatform) {
 
   let watchPositionParams = { timeout: 1000, enableHighAccuracy: true , maximumAge:1000};
   let compassAngle=0;
+  let currentAngle = 0;
   let pauseDraw=false;
 
   let orbs = [];
-  orbs.push(new Orb(100,200));
-  orbs.push(new Orb(100,600));
+  orbs.push(new Orb(50,100));
+  orbs.push(new Orb(-100,80));
 
   vm.coordinates={
     lon: 0,
@@ -22,26 +23,28 @@ function OrbViewController($scope, $ionicPlatform) {
     console.log("orbViewCtrl.js: device ready - getting compass");
     var locationWatchId = navigator.geolocation.watchPosition(geolocationSuccess, onError, watchPositionParams);
     var compassWatchID  = navigator.compass.watchHeading(compassSuccess, onError);
+    draw();
   });
 
   function compassSuccess(heading) {
     $scope.$apply(function() {
       vm.compass = heading;
-      if (pauseDraw==false){
-        compassAngle=parseInt(heading.magneticHeading);
-        pauseDraw=true;
-        draw();
-      }
+      compassAngle=parseInt(heading.magneticHeading)*Math.PI/180;
+      // if (pauseDraw==false){
+      //   compassAngle=parseInt(heading.magneticHeading);
+      //   pauseDraw=true;
+        //draw();
+      //}
     });
   }
 
   function geolocationSuccess(position) {
-    $scope.$apply(function() {
-      vm.coordinates={
-        lon: position.coords.longitude,
-        lat: position.coords.latitude
-      };
-    });
+    // $scope.$apply(function() {
+    //   vm.coordinates={
+    //     lon: position.coords.longitude,
+    //     lat: position.coords.latitude
+    //   };
+    // });
   }
 
   function onError(error) {
@@ -51,7 +54,42 @@ function OrbViewController($scope, $ionicPlatform) {
 
   function draw() {
     //canvas initialization
-    let angleRadian = -parseInt(compassAngle)*Math.PI/180;
+    //let currentAngle = -parseInt(compassAngle)*Math.PI/180;
+    $scope.$apply(function() {
+      vm.coordinates={
+        lon: currentAngle,
+        lat: "jee"
+      };
+    });
+
+    
+    let CCV = 2*Math.PI - compassAngle + currentAngle;
+    let CV = compassAngle - currentAngle;
+    let step = 0.01;
+
+    if(CCV > CV){
+      currentAngle -= step;
+    }else{
+      currentAngle += step;
+    }
+
+    if (Math.abs(currentAngle - compassAngle) < 1.5*step) {
+      currentAngle = compassAngle;
+      console.log("eka");
+    };
+
+    if (Math.abs(compassAngle - currentAngle) < 1.5*step) {
+      currentAngle = compassAngle;
+      console.log("toka");
+    };
+    // if (currentAngle < -2*Math.PI) {
+    //   currentAngle += 2*Math.PI;
+    // };
+
+    // if (currentAngle > 0) {
+    //   currentAngle -= 2*Math.PI;
+    // };
+
     let canvas = document.getElementById("myCanvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -60,14 +98,22 @@ function OrbViewController($scope, $ionicPlatform) {
 
     let maxSize = 20;
     let maxOpacity=1;
+    
+
+    ctx.translate(canvas.width/2, canvas.height/2);
+    ctx.rotate(-currentAngle);
+
     orbs.forEach(function(orb) {
-      let x = origon.x + (orb.x-origon.x)*Math.cos(angleRadian) - (orb.y-origon.y)*Math.sin(angleRadian);
-      let y = origon.y + (orb.x-origon.x)*Math.sin(angleRadian) + (orb.y-origon.y)*Math.cos(angleRadian);
       let distance = Math.sqrt( (orb.x-origon.x)*(orb.x-origon.x) + (orb.y-origon.y)*(orb.y-origon.x) );
-      drawCircle(ctx, x, y, scaleToDistance(maxSize,distance), scaleToDistance(maxOpacity,distance));
+      drawCircle(ctx, orb.x, orb.y, scaleToDistance(maxSize,distance), scaleToDistance(maxOpacity,distance));
     });
 
-    //    requestAnimationFrame(draw);
+    //drawCircle(ctx, 0, 0, 20, 100);
+    //drawCircle(ctx, 200, 20, 20, 100);
+    //drawCircle(ctx, 300, 10, 20, 100);
+
+
+    requestAnimationFrame(draw);
     pauseDraw=false;
   }
 
